d6c623a871bbf877f74d32138485ac04be55f18b
Anshal Ali <sherhasan543@gmail.com>
2024-08-20 22:40:15 +0500

    min coin sum

---
+++ problems/DynamicProgramming/min_coin_sum.js
@@ -0,0 +1,17 @@
+function minCoin(coins, amount, memo = {}) {
+    if (amount in memo) return memo[amount];
+    if (amount === 0) return 0;
+    if (amount < 0) return -1;
+    let minCoinCount = Infinity;
+    for (let coin of coins) {
+        let result = minCoin(coins, amount - coin, memo);
+        // console.log(result);
+        if (result !== -1) {
+            minCoinCount = Math.min(minCoinCount, result + 1);
+        }
+    }
+    memo[amount] = minCoinCount === Infinity ? -1 : minCoinCount;
+    return memo[amount];
+}
+
+console.log(minCoin([1, 2, 5], 11)); // 3
