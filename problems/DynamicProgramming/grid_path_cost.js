daa87ba92e8c1bf46e92875039712ab0e4a19b2b
Anshal Ali <sherhasan543@gmail.com>
2024-08-21 22:16:26 +0500

    dp

--- problems/DynamicProgramming/grid_path_cost.js
+++ problems/DynamicProgramming/grid_path_cost.js
@@ -5,24 +5,26 @@ function maxPathCost(grid, row = 0, col = 0, memo = {}) {
         col >= grid[0].length ||
         grid[row][col] === "X"
     ) {
-        return 0
+        return -Infinity
     }
     if (row === grid.length - 1 && col === grid[0].length - 1) {
-        return 1
+        return grid[row][col]
     }
     if (postion in memo) {
         return memo[postion]
     }
-
-    memo[postion] = maxPathCost(grid, row + 1, col, memo) + maxPathCost(grid, row, col + 1, memo)
+    const rightPath = maxPathCost(grid, row, col + 1, memo)
+    const downPath = maxPathCost(grid, row + 1, col, memo)
+    memo[postion] = grid[row][col] + Math.max(rightPath, downPath)
+    // memo[postion] = maxPathCost(grid, row + 1, col, memo) + maxPathCost(grid, row, col + 1, memo)
     return memo[postion]
 }
 
 const grid = [
-    [" ", "X", "X", "X"],
-    [" ", "X", " ", "X"],
-    [" ", " ", " ", " "],
-    ["X", "X", " ", " "],
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16],
 ]
 
 console.log(maxPathCost(grid)) 
